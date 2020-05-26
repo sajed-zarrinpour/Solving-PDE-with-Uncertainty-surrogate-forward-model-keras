3544b3d279b17513dffd7c1adf67a5c9e1382e9f
Sajed N. Zarinpour <samim56b@gmail.com,sa.zarrinpour@iasbs.ac.ir>
2020-05-26 13:41:41 +0430

    last run, make sure everything is in order, descriptions does not checked in this. just the code and results

--- ECIM/convertingSVGtouseinTEX.sh
+++ ECIM/convertingSVGtouseinTEX.sh
@@ -2,4 +2,4 @@
 #
 in=${1?Error: no input file name given}
 out=${2?Error: no output file name given}
-inkscape -D -z --file="${in}.svg" --export-pdf="${out}.pdf" --export-latex
+inkscape -D -z --file="${in}" --export-pdf="${out}.pdf" --export-latex
